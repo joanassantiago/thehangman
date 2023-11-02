159d1bc6f34c7f7bf5ccb35c4e52c00064b82e4c
Gajo <dinis.mvb@ua.pt>
2023-11-02 11:06:44 +0000

    Agora dá pra meter uma plavara inteira

--- thehangman.py
+++ thehangman.py
@@ -5,9 +5,8 @@ import random
 
 # Defenição das funções
 
-#EXTRA: se o utilizador introduzir uma palavra para tentar acertar fazer isso ya
+#So falta o boneco mas podemos faze-lo bonitao
 
-#falta you win, o desnho, you lose, contador de erros, lista de letras, meter pimpao e comentarios repeticao de erradas e de letras
 
 # Função que transforma caracteres especiais em normais
 def normaliseWord (word):
@@ -59,59 +58,82 @@ def hangmanGame(secret, hiddenWord, rawSecret):
         print("Letras disponíveis: ", ', '.join(letras_restantes))
 
         print(message)
-       
+        
         # O jogador escolhe uma letra
-        playerInput = input("Escolhe uma letra: ").upper()
+        playerInput = input("\nEscolhe uma letra (Se estiveres confiante, podes tentar adivinar a palavra): ").upper()
         
-        # Verificação de letras repetidas
-        if playerInput in letras_restantes:
-           
-            # Verificação de input válido
-            if str.isalpha(playerInput) == False:
-                message = "ERRO: Inválido."
-                continue
+        # Se o jogador quiser adivinhar apenas uma letra
+        if len(playerInput) == 1:   
+            # Verificação de letras repetidas
+            if playerInput in letras_restantes:
+            
+                # Verificação de input válido
+                if str.isalpha(playerInput) == False:
+                    message = "ERRO: Inválido."
+                    continue
+            
+                # Substituição (ou não no caso de errar) das letras.
+                else:
+                    # Valor que se permanecer a 0 contará um erro
+                    acertou = 0
+                    
+                    # Comparação de cada letra em secret com a letra inputed pelo player
+                    for index in range(len(secret)):
+                        if playerInput == rawSecret[index]:
+                            hiddenWord[index] = secret[index]
+                            acertou = 1
+                            letras_corretas += 1
+
+                            message = "Acertaste!"
+                            
+                            # Substituição da letra correta por um "+"
+                            if playerInput in letras_restantes:
+                                letras_restantes[letras_restantes.index(playerInput)] = "+"
+                                letras_usadas.append(playerInput)
+                    
+                    # Caso o valor "acertou" seja 0, significa que o player não acertou nenhuma letra, logo o valor de "erros" subirá por 1.
+                    if acertou == 0:
+                        erros += 1
+                        letras_restantes[letras_restantes.index(playerInput)] = "-"
+                        message = "Letra errada! Tenta outra vez!"
+                        letras_usadas.append(playerInput)
+                    
+
+                # Se o número de letras certas for do mesmo comprimento da palavra, o jogador vence
+                if letras_corretas == len(hiddenWord):
+                    print(3*"\n","GANHASTE!")
+                    print("Palavra: ", ''.join(secret))
+                    break
         
-            # Substituição (ou não no caso de errar) das letras.
+
+            # Caso o player escolha uma letra já escolhida
+            elif playerInput in letras_usadas:
+                message = "ERRO: Letra repetida."
+
+            # Caso o player apenas prima enter sem escrever nada
             else:
-                # Valor que se permanecer a 0 contará um erro
-                acertou = 0
-                
-                # Comparação de cada letra em secret com a letra inputed pelo player
-                for index in range(len(secret)):
-                    if playerInput == rawSecret[index]:
-                        hiddenWord[index] = secret[index]
-                        acertou = 1
-                        letras_corretas += 1
-
-                        message = "Acertaste!"
-                        
-                        # Substituição da letra correta por um "+"
-                        if playerInput in letras_restantes:
-                            letras_restantes[letras_restantes.index(playerInput)] = "+"
-                            letras_usadas.append(playerInput)
-                
-                # Caso o valor "acertou" seja 0, significa que o player não acertou nenhuma letra, logo o valor de "erros" subirá por 1.
-                if acertou == 0:
-                    erros += 1
-                    letras_restantes[letras_restantes.index(playerInput)] = "-"
-                    message = "Letra errada! Tenta outra vez!"
-                    letras_usadas.append(playerInput)
-                
-
-            # Se o número de letras certas for do mesmo comprimento da palavra, o jogador vence
-            if letras_corretas == len(hiddenWord):
-                print("\nGANHASTES!")
-                print("Palavra: ", ''.join(secret))
-                break
-
-        # Caso o player escolha uma letra já escolhida
-        elif playerInput in letras_usadas:
-            message = "ERRO: Letra repetida."
-
-        # Caso o player apenas prima enter sem escrever nada
-        else:
-           message = "ERRO: Inválido."
+                message = "ERRO: Inválido."
         
+        # Caso o player tente acertar a palavra toda de uma vez
+        elif len(playerInput) == len(hiddenWord):
+            if list(playerInput) == rawSecret: # Se as letras em playerInput forem as mesmas do que em rawSecret
+                print(3*"\n")
+                print("GANHASTE!")
+                print("WOW, adivinhaste a palavra por completo!")
+                break
+            # Será contado um erro se ele 
+            else:
+                message = "Ah! Estavas quase... A palavra não é essa."
+                erros += 1 
+        
+        elif playerInput not in letras_usadas:
+            message = "ERRO: Inválido."
+
+        else:
+            message = "ERRO: A palavra introduzida não tem o mesmo comprimeto do que a palavra escondida."
+
+    
+    
     # GameOver se o player chegar aos 7 erros        
     else:
         print(10* "\n")
@@ -135,6 +157,9 @@ def main():
    
     # Palavra secret (todos os caracteres tão em maiúscula)
     secret = random.choice(words).upper()
+
+    # Print de secret (apenas para troubleshooting)
+    # print(secret)
 
     #Palavra "secret" convertida em "_"
     hiddenWord = len(secret) * "_"
